c816bba29aa53c37894061ca611b5e88c4062eec
wansky05 <115464683+wansky05@users.noreply.github.com>
2025-03-10 06:31:57 +0700

    Create express.js

---
+++ netlify/functions/express.js
@@ -0,0 +1,5 @@
+// netlify/functions/express.js
+const serverless = require('serverless-http');
+const app = require('../../server'); // Mengimpor aplikasi Express
+
+module.exports.handler = serverless(app); // Ekspor fungsi serverless
