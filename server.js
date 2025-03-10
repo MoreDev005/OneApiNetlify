e1209827ff314a0a3d7191c32ba7792206e40695
Rurizora <bottoramku@gmail.com>
2025-03-10 07:11:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // server.js
 const express = require('express');
 const app = express();
+const read = require('./baca.js')
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.sendFile('./home.html');
+  let baca = await read('./home.html');
+  res.send(baca)
 });
 app.use("/.netlify/functions/express", router);
 module.exports = app; // Ekspor aplikasi Express
