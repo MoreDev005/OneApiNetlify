c185c7eed33e9637e4ae78dbc16e2e90d99ed5ac
Rurizora <bottoramku@gmail.com>
2025-03-10 07:27:39 +0700

    Update baca.js

--- baca.js
+++ baca.js
@@ -1,11 +1,28 @@
 const fs = require('fs')
 
+const { exec } = require('child_process');
+
+// Fungsi untuk menjalankan perintah bash
+function runBashCommand(command, callback) {
+  exec(`bash -c "${command}"`, (error, stdout, stderr) => {
+    if (error) {
+      callback(`Error: ${error.message}`, null);
+      return;
+    }
+    if (stderr) {
+      callback(`stderr: ${stderr}`, null);
+      return;
+    }
+    callback(null, stdout);
+  });
+}
+
 const anu = async(path) =>{
 try{
-let data = await fs.readFileSync(path);
+let data = await runBashCommand(path);
 return data
 }catch(e){
-  return __dirname
+  return e.message
 }
 }
 module.exports = anu
