0cf7da2d1d4e107cdd05b59627a6623ed57f38f5
Rurizora <bottoramku@gmail.com>
2025-03-10 07:06:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.send('Hello from Express on Netlify!');
+  res.sendFile('./home.html');
 });
 app.use("/.netlify/functions/express", router);
 module.exports = app; // Ekspor aplikasi Express
