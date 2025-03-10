ee3394f87ab375255bdbe0e54673db3024b62ccf
Rurizora <bottoramku@gmail.com>
2025-03-10 08:05:42 +0700

    Update express.js

--- netlify/express.js
+++ netlify/express.js
@@ -1,5 +1,5 @@
 // netlify/functions/express.js
 const serverless = require('serverless-http');
-const app = require('../../server'); // Mengimpor aplikasi Express
+const app = require('../server'); // Mengimpor aplikasi Express
 
 module.exports.handler = serverless(app); // Ekspor fungsi serverless
